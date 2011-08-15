ce5d952c1759194f5e1a709e2c7c62d1ee92117e
Matty Southall <matty-s@live.co.uk>
2011-08-15 17:00:23 +0100

    Made a mistake with the UserObjectComposer, fixed now and moved into correct place

--- Server/Communication/Outgoing/Handshake/UserObjectComposer.cs
+++ Server/Communication/Outgoing/Handshake/UserObjectComposer.cs
@@ -1,15 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
-namespace Snowlight.Communication.Outgoing.Handshake
+using Snowlight.Game.Sessions;
+using Snowlight.Game.Characters;
+
+namespace Snowlight.Communication.Outgoing
 {
     public static class UserObjectComposer
     {
-        public static ServerMessage Compose()
+        public static ServerMessage Compose(Session Session)
         {
-            return new ServerMessage(OpcodesOut.USER_OBJECT); // TODO: Find out what this actually does
+            ServerMessage Message = new ServerMessage(OpcodesOut.USER_OBJECT);
+            Message.AppendStringWithBreak(Session.CharacterId.ToString());
+            Message.AppendStringWithBreak(Session.CharacterInfo.Username);
+            Message.AppendStringWithBreak(Session.CharacterInfo.Figure);
+            Message.AppendStringWithBreak(Session.CharacterInfo.Gender == CharacterGender.Male ? "M" : "F");
+            Message.AppendStringWithBreak(Session.CharacterInfo.Motto);
+            Message.AppendStringWithBreak(Session.CharacterInfo.RealName);
+            Message.AppendInt32(0);
+            Message.AppendStringWithBreak("");
+            Message.AppendInt32(0);
+            Message.AppendInt32(0);
+            Message.AppendInt32(Session.CharacterInfo.RespectPoints);
+            Message.AppendInt32(Session.CharacterInfo.RespectCreditHuman);
+            Message.AppendInt32(Session.CharacterInfo.RespectCreditPets);
+            Message.AppendUInt32(24708);
+            return Message;
         }
     }
 }
